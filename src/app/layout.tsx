49d2deb8c69ba50f39ad3dc343bfa494b4a56207
dinnertime-who <dinnertime.dev@gmail.com>
2025-10-06 22:59:50 +0900

    VSCode 설정 파일 추가, 글로벌 CSS에 문서 스크롤 변수 추가, 레이아웃 컴포넌트에서 폰트 변경 및 최적화, Pretendard 폰트 파일 및 관련 코드 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ReactQueryProvider } from "@/components/react-query/react-query-provider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { pretendard } from "@/fonts";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="ko" className="scroll-smooth scroll-pt-(--document-scroll-pt)">
+      <body className={cn(pretendard.className, pretendard.variable)}>
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
     </html>
